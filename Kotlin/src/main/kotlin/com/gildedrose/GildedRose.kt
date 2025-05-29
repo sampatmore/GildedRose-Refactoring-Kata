036912a71854b2de1bb03cecd6e16ca3f0a49883
Samuel Patmore <sam.patmore@so.energy>
2025-05-29 16:24:58 +0100

    removed logic

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -65,7 +65,7 @@ class GildedRose(val items: List<Item>) {
         item.sellIn = item.sellIn - 1
 
         if (item.sellIn < 0) {
-            item.quality = item.quality - item.quality
+            item.quality = 0
         }
 
     }
