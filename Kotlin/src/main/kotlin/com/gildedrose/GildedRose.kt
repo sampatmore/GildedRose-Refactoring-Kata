27b438e3faf1d5c8efc46f9842bdac3c655616e7
Samuel Patmore <sam.patmore@so.energy>
2025-05-29 16:29:03 +0100

    removed logic

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -16,53 +16,40 @@ class GildedRose(val items: List<Item>) {
     }
 
     private fun updateItemQuality(item: Item) {
-            if (item.quality > 0) item.quality = item.quality - 1
-            item.sellIn = item.sellIn - 1
+        if (item.quality > 0) item.quality--
+            item.sellIn--
 
-        if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                            item.quality = item.quality - 1
-                    }
-        }
+        if (item.sellIn < 0 && item.quality > 0) item.quality--
     }
 
     private fun updateAgedBrie(item: Item) {
         if (item.name != "Aged Brie") return
 
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-        }
-        item.sellIn = item.sellIn - 1
+        if (item.quality < 50) item.quality++
+        item.sellIn--
 
-        if (item.sellIn < 0 && item.quality < 50) {
-            item.quality = item.quality + 1
-        }
+        if (item.sellIn < 0 && item.quality < 50) item.quality++
+
     }
 
     private fun updateSulfuras(item: Item) {
         if (item.name != "Sulfuras, Hand of Ragnaros") return
 
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-        }
+        if (item.quality < 50) item.quality++
     }
 
     private fun updateBackstage(item: Item) {
         if (item.name != "Backstage passes to a TAFKAL80ETC concert") return
 
         if (item.quality < 50) {
-            item.quality = item.quality + 1
+            item.quality++
 
-                if (item.sellIn < 11 && item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
+                if (item.sellIn < 11 && item.quality < 50) item.quality++
 
-                if (item.sellIn < 6 && item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
+                if (item.sellIn < 6 && item.quality < 50) item.quality++
         }
 
-        item.sellIn = item.sellIn - 1
+        item.sellIn--
 
         if (item.sellIn < 0) {
             item.quality = 0
