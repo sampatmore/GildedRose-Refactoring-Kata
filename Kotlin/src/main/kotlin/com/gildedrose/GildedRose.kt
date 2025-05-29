2c10389ec9d1a60f9ea1fa9e6e4be5d4c40884ed
Samuel Patmore <sam.patmore@so.energy>
2025-05-29 16:11:34 +0100

    extracted updateBackstage function

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,7 +7,8 @@ class GildedRose(val items: List<Item>) {
             val item = items[i]
             updateAgedBrie(item)
             updateSulfuras(item)
-            if (item.name != "Aged Brie" && item.name != "Sulfuras, Hand of Ragnaros") {
+            updateBackstage(item)
+            if (item.name != "Aged Brie" && item.name != "Sulfuras, Hand of Ragnaros" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
                 updateItemQuality(item)
             }
 
@@ -78,5 +79,30 @@ class GildedRose(val items: List<Item>) {
             item.quality = item.quality + 1
         }
     }
+
+    private fun updateBackstage(item: Item) {
+        if (item.name != "Backstage passes to a TAFKAL80ETC concert") return
+
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+
+                if (item.sellIn < 11 && item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+
+                if (item.sellIn < 6 && item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+        }
+
+        item.sellIn = item.sellIn - 1
+
+        if (item.sellIn < 0) {
+            item.quality = item.quality - item.quality
+        }
+
+    }
+
+
 }
 
