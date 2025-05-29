eae6aa890c9214ce513d7fa2726b7cd214f715c0
Samuel Patmore <sam.patmore@so.energy>
2025-05-29 15:08:33 +0100

    first change

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,26 +5,20 @@ class GildedRose(val items: List<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
+                if (items[i].quality > 0 && items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    items[i].quality = items[i].quality - 1
                 }
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
 
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
-                            }
                         }
                     }
                 }
